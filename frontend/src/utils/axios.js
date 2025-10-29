381cff7406f0ee8d0a11f76ed38e6da8557660c2
Harshithav96 <1si23cs064@sit.ac.in>
2025-10-29 22:45:31 +0530

    Added start script for Render

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import qs from "qs";
 
-// Serialization means converting a JavaScript object or array into a URL query string that can be sent in an HTTP request.
+// Base URL for local + production
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 export const axiosInstance = axios.create({
-  baseURL:"/api",
+  baseURL: `${BASE_URL}/api`, // ✅ keep /api because your API calls start with /v1/rent
   withCredentials: true,
   paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
 });
