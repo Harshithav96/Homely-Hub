4cbeba53ba32ec58d3a96f52fe231257ab65abaa
Harshithav96 <1si23cs064@sit.ac.in>
2025-10-29 22:54:02 +0530

    Added start script for Render

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import qs from "qs";
 
 // Base URL for local + production
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL|| "http://localhost:8080";
 
 export const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api`, // ✅ keep /api because your API calls start with /v1/rent
